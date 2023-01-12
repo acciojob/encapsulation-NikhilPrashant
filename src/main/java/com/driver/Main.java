d6630123d18bf243dd173d148936229927cc1d35
Nikhil Prashant <nikhilprashant1998@gmail.com>
2023-01-12 16:21:41 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWOnly newPerson = new RWOnly();
+        // these will not work because the name variable is private
+        // newPerson.name = "Name";
+        // System.out.println(newPerson.name);
+        newPerson.setName("Name");
+        System.out.println(newPerson.getName());
+    }
 }
